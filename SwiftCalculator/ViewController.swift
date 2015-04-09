f4b84c92c0af512239358d6fd97f66c70c946489
Huan.Wang <freddie218@163.com>
2015-04-08 19:40:32 -0500

    finish the basic function for lecture 1 and 2. huan

--- SwiftCalculator/ViewController.swift
+++ SwiftCalculator/ViewController.swift
@@ -10,6 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    var numbersToBeCalculted = Array<String>()
+    
+    @IBAction func clickButton(sender: UIButton) {
+        let clickedButton = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            display.text = display.text! + clickedButton
+        } else {
+            display.text = clickedButton
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        var operatorClicked = sender.currentTitle!
+        var result = Double()
+        
+        switch operatorClicked {
+        case "↵" : numbersToBeCalculted.append(display.text!)
+        case "+" : calculate({ $1 + $0 })
+        case "−" : calculate({ $1 - $0 })
+        case "×" : calculate({ $1 * $0 })
+        case "÷" : calculate({ $1 / $0 })
+        case "√" : calculate({ sqrt($0) })
+        default : break
+        }
+
+        userIsInTheMiddleOfTyping = false
+        
+        println("numbersToBeCalculted is: \(numbersToBeCalculted)")
+    }
+    
+    func calculate(binaryCal: (num1:Double, num2:Double) -> Double){
+        if numbersToBeCalculted.count >= 2 {
+            let number1 = NSNumberFormatter().numberFromString(numbersToBeCalculted.removeLast())!.doubleValue
+            let number2 = NSNumberFormatter().numberFromString(numbersToBeCalculted.removeLast())!.doubleValue
+            var result = binaryCal(num1: number1, num2: number2)
+            
+            display.text = "\(result)";
+            numbersToBeCalculted.append("\(result)")
+        }
+    }
+    
+    func calculate(singularCal: (num1:Double) -> Double) {
+        if numbersToBeCalculted.count >= 1 {
+            let number1 = NSNumberFormatter().numberFromString(numbersToBeCalculted.removeLast())!.doubleValue
+            var result = singularCal(num1: number1)
+            
+            display.text = "\(result)";
+            numbersToBeCalculted.append("\(result)")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
